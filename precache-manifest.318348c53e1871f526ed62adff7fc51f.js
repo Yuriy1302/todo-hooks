117889ddee3644d28c6aa122af0530a971f867d7
Yuriy13202 <ascendo@list.ru>
2020-09-11 20:19:59 +0700

    Updates

---
+++ precache-manifest.318348c53e1871f526ed62adff7fc51f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f91a1810b5848eb629e871d7b36a0ca6",
+    "url": "/todo-hooks/index.html"
+  },
+  {
+    "revision": "550c40684a60ea92418d",
+    "url": "/todo-hooks/static/css/main.63b5e69b.chunk.css"
+  },
+  {
+    "revision": "f89dc41798b2d353c06f",
+    "url": "/todo-hooks/static/js/2.f4a3f2fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-hooks/static/js/2.f4a3f2fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "550c40684a60ea92418d",
+    "url": "/todo-hooks/static/js/main.075edb32.chunk.js"
+  },
+  {
+    "revision": "0e4c6f6b07b6b858bd5c",
+    "url": "/todo-hooks/static/js/runtime-main.4b3f997b.js"
+  }
+]);
